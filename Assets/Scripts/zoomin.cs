fbffcff4e832334a8324fc702a85de0fb5b49620
whereistt <mac@pc-129-31.customer.ask4.lan>
2021-03-10 18:49:04 +0000

    dd

---
+++ Assets/Scripts/zoomin.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+//Script for moving a gameObject smoothly
+//Usage: Attach the character controller component to the gameobject that you want to move
+
+namespace UnityLibary
+{
+    public class zoomin : MonoBehaviour
+    {
+        // place the gameobject that you want to move to the controller placeholder
+        public CharacterController controller;
+
+        public float speed = 5f;
+
+        void Update()
+        {
+            float z = Input.GetAxis("Vertical");
+
+            Vector3 move = transform.forward * z;
+
+            controller.Move(move * speed * Time.deltaTime);
+
+            //Rotate clockwise
+            if (Input.GetKey(KeyCode.E))
+            {
+                transform.RotateAround(transform.position, Vector3.up, 100 * Time.deltaTime);
+            }
+
+            // Rotate anticlockwise
+            if (Input.GetKey(KeyCode.Q))
+            {
+                transform.RotateAround(transform.position, -Vector3.up, 100 * Time.deltaTime);
+            }
+
+        }
+
+    }
+}
